093aaf7a28cf1b416026efe0ae119663d2c55ccb
Yixiong Fang <6135847+hayyyyyyden@users.noreply.github.com>
2020-05-27 22:47:22 +1000

    Done some UI work!

--- Lab8-Git/ViewController.swift
+++ Lab8-Git/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("done nothing.")
+        print("Done some UI work!")
         // Do any additional setup after loading the view.
     }
 
